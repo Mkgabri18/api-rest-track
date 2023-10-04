f763628f8b6f7c6910866f6149ec04a29f7bc75d
gabriel carrasco <carrasco.gabriel136@gmail.com>
2023-10-04 16:43:56 +0200

    apply order require route

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const routes = require('./routes')
 const cors = require('cors')
 const dbConnect = require("./config/mongo");
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static("storage"))
+app.use(express.static("storage")) //* path static files to send
 
 const port = process.env.PORT || 3000
 
-app.use("/api",  require("./routes"))
+app.use("/api",  routes)
 
 app.listen(port, () => {
     console.log("app ready on http://localhost:"+port)
